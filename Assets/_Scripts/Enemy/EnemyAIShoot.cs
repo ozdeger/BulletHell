54d774ec116605cfb0f9621afec7825bce1086ae
talhaucarr <talhaucarr@hotmail.com>
2021-05-22 03:17:54 +0300

    enemy shoot ai

---
+++ Assets/_Scripts/Enemy/EnemyAIShoot.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class EnemyAIShoot : MonoBehaviour
+{
+
+    [SerializeField] private float range;
+
+    private GunController _gunController;
+    private Camera _camera;
+    private Collider2D _collider;
+
+    private Vector2 playerColliderCenter;
+
+   
+    void Start()
+    {
+        _gunController = GetComponent<GunController>();
+        _camera = CameraManager.Instance.Camera;
+    }
+    void Update()
+    {
+        GetLine();
+        TakeSightInput();
+    }
+
+    private void GetLine()
+    {
+        playerColliderCenter = PlayerManager.Instance.Player.GetComponent<Collider2D>().bounds.center;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, (playerColliderCenter - (Vector2)transform.position).normalized, range);
+          
+        if (hit.collider.gameObject.GetComponent<Tag>().Tags.Contains(Tags.Player))
+        {
+            _gunController.test();
+        }
+
+        else
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+        }
+    }
+
+    private void TakeSightInput()
+    {
+        Vector3 mousePosition = _camera.ScreenToWorldPoint(playerColliderCenter);
+        mousePosition.z = 0;
+        Vector3 aimDirection = (playerColliderCenter - (Vector2)transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        _gunController.TakeSight(new Vector3(0, 0, angle));
+    }
+}
